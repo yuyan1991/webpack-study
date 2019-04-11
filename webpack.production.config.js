4aacb82d6d0fbace2eeb6aa73a5d66322d8cc83d
yuyan <yuyan1991@gmail.com>
2019-04-11 17:50:51 +0800

    add cache config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
       path: __dirname + "/build",//打包后的文件存放的地方
-      filename: "bundle.js"//打包后输出文件的文件名
+      filename: "bundle-[hash].js"//打包后输出文件的文件名
     },
     devServer: {
       contentBase: "./public",//本地服务器所加载的页面所在的目录
