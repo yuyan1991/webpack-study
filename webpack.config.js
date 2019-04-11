b9e782a71bb709603d0353c0723287720e249ddd
yuyan <yuyan1991@gmail.com>
2019-04-11 17:21:13 +0800

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
-      path: __dirname + "/public",//打包后的文件存放的地方
+      path: __dirname + "/build",//打包后的文件存放的地方
       filename: "bundle.js"//打包后输出文件的文件名
     },
     devServer: {
@@ -44,6 +45,10 @@ module.exports = {
       maxAssetSize: 512000
     },
     plugins: [
-        new webpack.BannerPlugin('版权所有，翻版必究')
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+          template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin()//热加载插件
     ]
 }
