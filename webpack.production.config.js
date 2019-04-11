ef3c96015f93c7499e4888340078cd12f9facfc0
yuyan <yuyan1991@gmail.com>
2019-04-11 17:49:20 +0800

    add production config

---
+++ webpack.production.config.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    devtool: 'null', //注意修改了这里，这能大大压缩我们的打包代码
+    entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+      path: __dirname + "/build",//打包后的文件存放的地方
+      filename: "bundle.js"//打包后输出文件的文件名
+    },
+    devServer: {
+      contentBase: "./public",//本地服务器所加载的页面所在的目录
+      historyApiFallback: true,//不跳转
+      inline: true,//实时刷新
+      port: 2333
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use:  ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                          modules: true, // 指定启用css modules
+                          localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+                        }
+                    }, {
+                        loader: "postcss-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    },
+    optimization: {
+        minimizer: [
+          // we specify a custom UglifyJsPlugin here to get source maps in production
+          new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            uglifyOptions: {
+              compress: false,
+              ecma: 6,
+              mangle: true
+            },
+            sourceMap: true
+          })
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+          template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new ExtractTextPlugin("style.css")
+    ]
+}
